a8586584f42bbf8cf439540daeb26e42fd68f45a
subash <subashs@aarnanetworks.com>
2024-01-31 06:52:47 +0000

    changes as part of the testing

--- ran-operator/app/kopf_operator.py
+++ ran-operator/app/kopf_operator.py
@@ -1,19 +1,30 @@
 import kopf
+import datetime
+import random
+import logging
+@kopf.on.probe(id='now')
+def get_current_timestamp(**kwargs):
+    return datetime.datetime.utcnow().isoformat()
+
+@kopf.on.probe(id='random')
+def get_random_value(**kwargs):
+    return random.randint(0, 1_000_000)
 
 # When creating or resuming object
-@kopf.on.resume('example.com', 'v1alpha1', 'ran')
-@kopf.on.create('example.com', 'v1alpha1', 'ran')
+@kopf.on.resume('ranoperator.example.com', 'v1alpha1', 'ranoperator')
+@kopf.on.create('ranoperator.example.com', 'v1alpha1', 'ranoperator')
 def create_fn(spec, name, namespace, logger, **kwargs):
-    print(f"Creating: {spec}")
+    logging.info(f"Creating: {spec}")
     data = hello_world(spec, name)
-    print(data)
+    logging.info(data)
   
 
-@kopf.on.delete('example.com', 'v1alpha1', 'ran')
+@kopf.on.delete('ranoperator.example.com', 'v1alpha1', 'ranoperator')
 def delete_fn(spec, name, status, namespace, logger, **kwargs):
-    print(f"Deleting: {spec}")
+    logging.info(f"Deleting: {spec}")
+    logging.info(f"status {status}")
     
 def hello_world(spec, name):
-    print(f"Hello World: {spec}")
-    print(f"Hello World: {name}")
+    logging.info(f"Hello World: {spec}")
+    logging.info(f"Hello World: {name}")
     return {'hello': 'world'}

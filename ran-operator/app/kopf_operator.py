45ce9cde9eb9a03663a7183817fac9b965238403
Subash <subashs@aarnanetworks.com>
2024-02-07 15:02:01 +0530

    changes as part of the testing

--- ran-operator/app/kopf_operator.py
+++ ran-operator/app/kopf_operator.py
@@ -2,6 +2,8 @@ import kopf
 import datetime
 import random
 import logging
+
+
 @kopf.on.probe(id='now')
 def get_current_timestamp(**kwargs):
     return datetime.datetime.utcnow().isoformat()
@@ -14,17 +16,23 @@ def get_random_value(**kwargs):
 @kopf.on.resume('ranoperator.example.com', 'v1alpha1', 'ranoperator')
 @kopf.on.create('ranoperator.example.com', 'v1alpha1', 'ranoperator')
 def create_fn(spec, name, namespace, logger, **kwargs):
-    logging.info(f"Creating: {spec}")
+    logger.info(f"Creating: {spec}")
     data = hello_world(spec, name)
-    logging.info(data)
+    logger.info(data)
   
+@kopf.on.update('ranoperator.example.com', 'v1alpha1', 'ranoperator')
+def modify_fn(spec, name, namespace, logger, **kwargs):
+    logger.info(f"Modifying: {spec}")
+    data = hello_world(spec, name)
+    logger.info(data)
 
 @kopf.on.delete('ranoperator.example.com', 'v1alpha1', 'ranoperator')
-def delete_fn(spec, name, status, namespace, logger, **kwargs):
-    logging.info(f"Deleting: {spec}")
-    logging.info(f"status {status}")
+def delete_fn(spec, name, logger, **kwargs):
+    logger.info(f"Deleting: {spec}")
     
 def hello_world(spec, name):
     logging.info(f"Hello World: {spec}")
     logging.info(f"Hello World: {name}")
     return {'hello': 'world'}
+
+
